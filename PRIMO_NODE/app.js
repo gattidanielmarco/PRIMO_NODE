64bcbfb1f1236301c033887451b62d65dd999ec5
gattidanielmarco <gatti.daniel.marco@iisgalvanimi.edu.it>
2019-10-28 10:46:26 +0000

    Quasi finito

--- PRIMO_NODE/app.js
+++ PRIMO_NODE/app.js
@@ -3,6 +3,10 @@ var app = express();
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
+app.get('/api/products',function(req,res){
+ res.sendFile(path.join(__dirname+'/products.json'));
+  //__dirname : Ritorna la cartella del progetto
+});
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
